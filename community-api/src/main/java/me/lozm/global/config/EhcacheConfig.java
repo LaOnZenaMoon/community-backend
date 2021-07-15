f968180bf6585238b7652775563b841a10ffae59
junlee <laonzenamoon@gmail.com>
2021-07-15 19:37:06 +0900

    feat: EhCache 라이브러리 및 관련 로직 수정

--- community-api/src/main/java/me/lozm/global/config/EhcacheConfig.java
+++ community-api/src/main/java/me/lozm/global/config/EhcacheConfig.java
@@ -1,12 +1,25 @@
 package me.lozm.global.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.sf.ehcache.CacheManager;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Slf4j
 @EnableCaching
-public class EhcacheConfig {
+@Configuration
+@RequiredArgsConstructor
+public class EhcacheConfig implements CommandLineRunner {
 
+    private final CacheManager cacheManager;
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("\n\n" + "=========================================================\n"
+                + "Using cache manager: " + this.cacheManager.getClass().getName() + "\n"
+                + "=========================================================\n\n");
+    }
 
 }
